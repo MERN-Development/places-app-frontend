1005f999c9192466ca23ec2d39ae86f8d9060702
Brayan Garcia <bgarcia.95@outlook.com>
2020-06-20 16:04:10 -0600

    Adding UsersList component and dummy data to Users page

--- src/user/pages/Users.jsx
+++ src/user/pages/Users.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import UsersList from "../components/UsersList";
 
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Brayan García",
+      image:
+        "https://www.biography.com/.image/ar_8:10%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_620/MTcyMzE0MzI2NTU0NjQ5ODEy/ang-lee-gettyimages-163118045.jpg",
+      places: 3,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
